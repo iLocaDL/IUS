85b508b45669a9fdfef15403325a39be0156b104
iLocaDL <lorenzolocatelli2003@gmail.com>
2025-12-03 10:41:55 +0100

    Add files via upload

---
+++ Sito ius/src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import SquadreIUS from './pages/SquadreIUS';
+import Eventi from './pages/Eventi';
+import Safeguarding from './pages/Safeguarding';
+import ChiSiamo from './pages/ChiSiamo';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+
+  const renderPage = () => {
+    const props = { searchQuery };
+
+    switch (currentPage) {
+      case 'Home':
+        return <Home {...props} />;
+      case 'Squadre IUS':
+        return <SquadreIUS {...props} />;
+      case 'Eventi':
+        return <Eventi {...props} />;
+      case 'Safeguarding':
+        return <Safeguarding />;
+      case 'Chi siamo?':
+        return <ChiSiamo />;
+      default:
+        return <Home {...props} />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <Header currentPage={currentPage} onPageChange={setCurrentPage} onSearch={handleSearch} />
+      <main className="flex-grow">
+        {renderPage()}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
